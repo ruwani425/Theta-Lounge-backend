177f9ef28614bed0285ca790039adf878bf59930
ruwani <ruwanielv@gmail.com>
2025-12-08 18:24:02 +0530

    modify corn job

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,25 @@
+// server.ts
+
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import app from "./app";
 import seedAdmin from "./script/seedAdmin";
+// 🆕 1. Import the cron job starting function
+import { startExpirationCronJob } from "./controllers/package-activation.controller"; 
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URL!)
-  .then(async () => {
-    console.log("MongoDB Connected");
+  .connect(process.env.MONGO_URL!)
+  .then(async () => {
+    console.log("MongoDB Connected");
 
-    await seedAdmin();
+    await seedAdmin();
 
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.log("DB Error: ", err));
+    startExpirationCronJob(); 
+
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log("DB Error: ", err));
