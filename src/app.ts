1d8de66e65bf56c744c7a680c2f4eeade46716f9
ruwani <ruwanielv@gmail.com>
2026-01-06 13:16:54 +0530

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200 
   })
